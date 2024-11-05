1069f92b4d7e2dfe6a1f81fffe064b6f8b0fbc73
yakhiyayeva <231469@astanait.edu.kz>
2024-11-05 14:56:33 +0500

    Add t_bot package with basic Telegram bot setup and message update function

---
+++ memory-game/internal/bot.go
@@ -0,0 +1,18 @@
+package t_bot
+
+import (
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/tucnak/telebot.v2"
+)
+
+type Bot struct {
+	TgBot *telebot.Bot
+	Chat  *telebot.Chat
+}
+
+func (b *Bot) Update(message string) {
+	_, err := b.TgBot.Send(b.Chat, message)
+	if err != nil {
+		log.Errorf("Error while sending message in observe: %v", err)
+	}
+}
